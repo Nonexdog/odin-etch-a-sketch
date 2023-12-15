51e5f9efe120f66a703804ede6e6e55a96c6f15b
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2023-12-15 17:35:47 -0600

    Modify addEventListener for squares

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ for(let rows = 1; rows <= rowsAndColumns; rows++) {
   for(let columns = 1; columns <= rowsAndColumns; columns++) {
     const squareEtch = document.createElement('div');
     squareEtch.style.cssText = 'border: 1px solid black; z-index: 100';
-    squareEtch.addEventListener('mouseover', (event) => {
-      addColor(event.currentTarget,0);
+    squareEtch.addEventListener('mouseover', () => {
+      addColor(squareEtch, 0);
     })
     squareEtch.style.width = 100/rowsAndColumns +'%';
     squareEtch.style.height = 100/rowsAndColumns +'%';
