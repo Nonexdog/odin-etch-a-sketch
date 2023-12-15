d10ef9a7b300fd3cd66a42f1e1b7f94aa403b823
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2023-12-15 16:44:32 -0600

    Add hover functionality that colors squares black

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const gridEtch = document.querySelector('.sketch-container');
+const colorList = ['colorBlack'];
 let rowsAndColumns = 16;
 
 for(let rows = 1; rows <= rowsAndColumns; rows++) {
   for(let columns = 1; columns <= rowsAndColumns; columns++) {
-    let squareEtch = document.createElement('div');
+    const squareEtch = document.createElement('div');
     squareEtch.style.cssText = 'border: 1px solid black; z-index: 100';
+    squareEtch.addEventListener('mouseover', (event) => {
+      addColor(event.currentTarget,0);
+    })
     squareEtch.style.width = 100/rowsAndColumns +'%';
     squareEtch.style.height = 100/rowsAndColumns +'%';
     gridEtch.appendChild(squareEtch);
   }
+}
+
+function addColor(target, color = 0) {
+  target.setAttribute('class', colorList[color]);
 }
