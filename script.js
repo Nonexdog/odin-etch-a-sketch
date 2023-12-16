6108c24b831320318d8e0e9ae02efa698a8c77dc
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2023-12-15 18:24:25 -0600

    Add clear grid functionality to button

--- script.js
+++ script.js
@@ -2,19 +2,31 @@ const gridEtch = document.querySelector('.sketch-container');
 const colorList = ['colorBlack'];
 let rowsAndColumns = 16;
 
-for(let rows = 1; rows <= rowsAndColumns; rows++) {
-  for(let columns = 1; columns <= rowsAndColumns; columns++) {
-    const squareEtch = document.createElement('div');
-    squareEtch.style.cssText = 'border: 1px solid black; z-index: 100';
-    squareEtch.addEventListener('mouseover', () => {
-      addColor(squareEtch, 0);
-    })
-    squareEtch.style.width = 100/rowsAndColumns +'%';
-    squareEtch.style.height = 100/rowsAndColumns +'%';
-    gridEtch.appendChild(squareEtch);
+function generateGrid() {
+  for(let rows = 1; rows <= rowsAndColumns; rows++) {
+    for(let columns = 1; columns <= rowsAndColumns; columns++) {
+      const squareEtch = document.createElement('div');
+      squareEtch.style.cssText = 'border: 1px solid black; z-index: 100';
+      squareEtch.addEventListener('mouseover', () => {
+        addColor(squareEtch, 0);
+      })
+      squareEtch.style.width = 100/rowsAndColumns +'%';
+      squareEtch.style.height = 100/rowsAndColumns +'%';
+      gridEtch.appendChild(squareEtch);
+    }
   }
 }
-
 function addColor(target, color = 0) {
   target.setAttribute('class', colorList[color]);
 }
+
+const buttonNewGrid = document.querySelector('#newGrid');
+const buttonClearGrid = document.querySelector('#clearGrid');
+
+buttonClearGrid.addEventListener('click', () => {
+  for (child of gridEtch.children) {
+    child.className = '';
+  }
+})
+
+generateGrid();
